ac11e14def4ccbca39b9fe539116925c96750b2e
Jonathan Tolbert <imanjesolutions@gmail.com>
2017-02-22 01:48:58 -0500

    I am currently passing the contact data to my saveQuote funciont. I need to make a createQuote service and inject it into my contact-list.js

--- app/components/contact-list.js
+++ app/components/contact-list.js
@@ -2,29 +2,74 @@ import Ember from 'ember';
 
 export default Ember.Component.extend({
    store: Ember.inject.service(),
+    selectedQuote:   '',
 
     init(){
         this._super();
         this.initialize();
-        //let store = this.get('store');
 
-        //let contactlist = store.findAll('contacts');
-
-       // console.log("The contact list: " + contactlist);
     },
-    initialize() {
-        this.contactList().then((data) => {
 
+
+    initialize() {
+        //sets the promise
+        this.contactList().then((data) => {
+            //the callback
             this.set('contactEntries', data);
         });
     },
 
     contactList(){
-        const store = this.get('store');
-
+          const store = this.get('store');
 
         return store.findAll('contacts');
+    },
+
+    actions:{
+        getID: function(){
+          let id =  Ember.$('input[name=optradio]:checked').val();
+              //this.get('selectedQuote');
+            console.log("The Id is: ",id );
+            this.send('getContact', id);
+        },
+
+        getContact: function(id){
+            let contactId = id;
+            console.log("Get Contact: ",contactId );
+           // const store = this.get('store');
+            //let contact = store.findRecord('contacts' ,contactId);
+
+            this.contactFound(contactId).then((data) => {
+               var  contactData =  data;
+
+                console.log("The contact is: ", contactData);
+                this.send('saveQuote', contactData);
+
+                    //this.set('contactData', data);
+            });
+
+
+        },
+
+        saveQuote(contactData){
+            console.log("YOu called saveQuote:", contactData);
+        }
+
+    },
+
+    contactFound(contactId){
+        console.log("You called Contact Found");
+
+        const store = this.get('store');
+        return store.findRecord('contacts' ,contactId);
+
     }
+
+
+
+
+
+
 
 
 
